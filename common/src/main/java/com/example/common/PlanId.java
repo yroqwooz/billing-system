3f0e0558c8f3ef28f69b2730b19b4f9f428d8bed
yronwooz <yronwooz@gmail.com>
2025-12-23 13:34:13 +0300

    [Common] PlanId.java and UserId.java methods

--- common/src/main/java/com/example/common/PlanId.java
+++ common/src/main/java/com/example/common/PlanId.java
@@ -6,4 +6,8 @@ public record PlanId(UUID value) {
     public static PlanId newId() {
         return new PlanId(UUID.randomUUID());
     }
+
+    public static PlanId fromString(String uuid) {
+        return new PlanId(UUID.fromString(uuid));
+    }
 }
--- common/src/main/java/com/example/common/UserId.java
+++ common/src/main/java/com/example/common/UserId.java
@@ -2,4 +2,12 @@ package com.example.common;
 
 import java.util.UUID;
 
-public record UserId(UUID value) {}
+public record UserId(UUID value) {
+    public static UserId newId() {
+        return new UserId(UUID.randomUUID());
+    }
+
+    public static UserId fromString(String uuid) {
+        return new UserId(UUID.fromString(uuid));
+    }
+}
